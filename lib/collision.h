2a2ee1515417265040ef7e2744c5173a394de362
Elouan230 <elouan2205@gmail.com>
2026-02-11 16:12:57 +0100

    Add files via upload

---
+++ lib/collision.h
@@ -0,0 +1,15 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+
+#include <player.h>
+#include <SDL/SDL.h>
+
+
+int collision( SDL_Rect a, SDL_Rect b);
+
+void HorizontaleCollision (Player * player, SDL_Rect * plateforms, int platformCount, int moveX);
+
+void VerticaleCollision (Player * player, SDL_Rect * plateforms, int platformCount);
+
+#endif

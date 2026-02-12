4592a1b3d0a2fd4f2137e2e861d0f598ddd4003d
Theo1804 (Ernault Théo) <116352576+Theo18004@users.noreply.github.com>
2026-02-12 13:25:49 +0100

    Delete src/physics.c

--- src/physics.c
+++
@@ -1,9 +0,0 @@
-/**
- * @file physics.c
- * @brief Implémentation du moteur physique.
- * @internal
- * Regroupe les fonctions de calcul de gravité, de gestion de la vitesse 
- * et de résolution des collisions mur/sol.
- */
-
-/* Gestion des collision (mur et sol), Gravité pour les sauts*/

7c9e9059bc6e33eb243bfb4560a866df3a2932a4
Alban_ANFRAY <alban.anfray.zeux@gmail.com>
2026-02-12 16:03:37 +0100

    Add files via upload

---
+++ doc/html/structCamera.js
@@ -0,0 +1,7 @@
+var structCamera =
+[
+    [ "h", "structCamera.html#a693b45b5fbfe6982649d1567549d7a7a", null ],
+    [ "w", "structCamera.html#ae92d4d04b7f0cbc7ae9769b6dbbc20d9", null ],
+    [ "x", "structCamera.html#af4142ba6c032bb34f4febeab8abae3ea", null ],
+    [ "y", "structCamera.html#a72b89a8abae63f1a2468a9a01bc3ee3b", null ]
+];
---
+++ doc/html/structMonkey.js
@@ -0,0 +1,11 @@
+var structMonkey =
+[
+    [ "animFPS", "structMonkey.html#a5db9eecb37c905b930b245f65dfbdf2c", null ],
+    [ "currentFrame", "structMonkey.html#a2d309cd2611c7b1ed2ca0c89e6b2b98c", null ],
+    [ "direction", "structMonkey.html#a70d2c2ca746e5508db05a4c53853e3c5", null ],
+    [ "frameTimer", "structMonkey.html#a26441a074edc1f4080ad883cd2b7263d", null ],
+    [ "position", "structMonkey.html#a07f660bdd7cb7cb79657c011e43d9714", null ],
+    [ "size", "structMonkey.html#acba43c5b40ece116771617a9f65ffb23", null ],
+    [ "speed", "structMonkey.html#a548565119e6f02b1fb9834486fc928f2", null ],
+    [ "spriteSheet", "structMonkey.html#acee53fd1cc62fd6f38fcaeeb2809f58e", null ]
+];

0ae41a9a08441d0e5ea5de7682f3ccd30bfad70b
Theo1804 (Ernault Théo) <116352576+Theo18004@users.noreply.github.com>
2026-03-03 11:35:57 +0100

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h> // Ne pas oublier cet include
+#include <SDL2/SDL_ttf.h> 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

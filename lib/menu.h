a4a0da3e8356b2a0a10a29bf9a1328ce6fcdce32
Elouan230 <elouan2205@gmail.com>
2026-03-03 11:54:05 +0100

    Add files via upload

--- lib/menu.h
+++ lib/menu.h
@@ -4,12 +4,26 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
+// Variables globales
+extern int volume;
+extern int luminosite;
+
 // Codes de retour du menu
 typedef enum {
     MENU_QUIT = 0,
     MENU_PLAY = 1,
     MENU_OPTIONS = 2
 } MenuResult;
+
+/**
+ * charge les parametres enregistrer
+ */
+void charger_parametres();
+
+/**
+ * sauvegarde les parametres
+ */
+void sauvegarder_parametres();
 
 /**
  * Affiche le menu et attend une action du joueur.
@@ -19,5 +33,16 @@ typedef enum {
  * @return L'action choisie (JOUER, QUITTER, etc.)
  */
 MenuResult afficher_menu(SDL_Renderer* renderer, int width, int height);
+
+
+/**
+ * Affiche le menu paramètre
+ * @param renderer Le renderer SDL déjà créé dans le main
+ * @param width Largeur réelle de la fenêtre
+ * @param height Hauteur réelle de la fenêtre
+ * @param font Police d'écriture
+ * @param textBG Image de fond
+ */
+void afficher_parametres(SDL_Renderer* renderer, int width, int height, TTF_Font* font, SDL_Texture* texBG);
 
 #endif

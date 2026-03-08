9d0f56634dc3904ad796b55de1d7b65f3eb1acc0
Mathis <mathis53600@gmail.com>
2026-03-08 22:13:54 +0100

    Add files via upload

--- lib/collision.h
+++ lib/collision.h
@@ -4,10 +4,21 @@
 #include <SDL.h>
 #include "defs.h"
 
-// Renvoie 1 si collision avec un mur, 0 sinon
+// Vérifie si le rectangle donné entre en collision avec des blocs solides sur la map
 int check_collision(SDL_Rect rect, int* map);
 
-// Renvoie 1 si le tile est un un bloc solide, 0 sinon
-int is_solid(int tile_id);
+//Renvoie 1 si la tuile est un bloc plein, 0 sinon
+int is_full(int tile_id);
+
+// Renvoie 1 si la tuile est un escalier vers la droite, 0 sinon
+int is_slope_up_right(int tile_id);
+
+// Renvoie 1 si la tuile est un escalier vers la gauche, 0 sinon
+int is_slope_up_left(int tile_id);
+
+// Renvoie 1 si la tuile est un escalier, 0 sinon
+int get_slope(int worldX, int tileX, int tileY, int tile_id);
+
+
 
 #endif

1ea4c8dd309e287fab026e35d6197103616724be
Mathis <mathis53600@gmail.com>
2026-03-08 02:51:57 +0100

    Add files via upload

--- src/camera.c
+++ src/camera.c
@@ -2,9 +2,10 @@
 #include "defs.h"
 
 void update_camera(Camera* cam, Player* p, int mapWidthPixel, int mapHeightPixel) {
+
     // Centrer sur le joueur
     cam->x = p->rect.x - cam->w / 2;
-    
+
     // Bloquer à gauche
     if (cam->x < 0) cam->x = 0;
     
@@ -12,7 +13,21 @@ void update_camera(Camera* cam, Player* p, int mapWidthPixel, int mapHeightPixel
     int maxScrollX = mapWidthPixel - cam->w;
     if (cam->x > maxScrollX) cam->x = maxScrollX;
 
-    // (Pour l'instant le scrollY est fixe en bas dans ton jeu, 
-    // mais on pourrait l'animer ici aussi si besoin)
-    cam->y = mapHeightPixel - cam->h;
+    // Camera de base fixé au sol
+    int base_y = mapHeightPixel - cam->h;
+    
+    // Décalage vertical 
+    int decallage_hauteur = 100; // Ajuste cette valeur pour changer la hauteur à laquelle la caméra commence à suivre le joueur
+    int camera_decalee_y = p->rect.y - decallage_hauteur; 
+
+    //Si le joueur est plus haut que la base, on monte la camera
+    if (camera_decalee_y < base_y) {
+        cam->y = camera_decalee_y; 
+    } else {
+        cam->y = base_y;
+    }
+    
+    // 4. Bloquer tout en haut pour ne pas sortir de la carte
+    if (cam->y < 0) cam->y = 0;
+
 }

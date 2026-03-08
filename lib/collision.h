9b2d6a241afa90a3153b13035bd5238b8c3ac64a
Mathis <mathis53600@gmail.com>
2026-03-08 02:15:40 +0100

    Add files via upload

--- lib/collision.h
+++ lib/collision.h
@@ -7,4 +7,7 @@
 // Renvoie 1 si collision avec un mur, 0 sinon
 int check_collision(SDL_Rect rect, int* map);
 
+// Renvoie 1 si le tile est un un bloc solide, 0 sinon
+int is_solid(int tile_id);
+
 #endif

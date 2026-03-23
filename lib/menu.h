5588a2a02798ada048f4d1d7ae918fa981a6d329
Alban_ANFRAY <alban.anfray.zeux@gmail.com>
2026-03-23 09:21:47 +0100

    Add files via upload

--- lib/menu.h
+++ lib/menu.h
@@ -1,27 +1,23 @@
-/**
- * @file menu.h
- * @brief Système de menus (principal, options) et gestion des paramètres.
- */
-
 #ifndef MENU_H
 #define MENU_H
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h> // Nécessaire pour Mix_Chunk
 
-extern int volume;      /**< Volume global du jeu */
-extern int luminosite;  /**< Luminosité de l'écran */
+extern int volume;      
+extern int luminosite;  
 
-/** @enum MenuResult */
 typedef enum {
-    MENU_QUIT = 0, /**< L'utilisateur veut quitter */
-    MENU_PLAY = 1, /**< L'utilisateur veut lancer la partie */
-    MENU_OPTIONS = 2 /**< L'utilisateur est dans les options */
+    MENU_QUIT = 0,
+    MENU_PLAY = 1,
+    MENU_OPTIONS = 2 
 } MenuResult;
 
 void charger_parametres();
 void sauvegarder_parametres();
 MenuResult afficher_menu(SDL_Renderer* renderer, int width, int height);
 void afficher_parametres(SDL_Renderer* renderer, int width, int height, TTF_Font* font, SDL_Texture* texBG);
+MenuResult afficher_pause(SDL_Renderer* renderer, int width, int height, Mix_Chunk* sfx);
 
 #endif

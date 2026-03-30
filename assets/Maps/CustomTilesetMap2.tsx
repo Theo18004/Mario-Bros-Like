b4a9c54b7af83fe6f61f383b2e1000703d51a597
Mathis <mathis53600@gmail.com>
2026-03-30 09:19:09 +0200

    Add files via upload

--- assets/Maps/CustomTilesetMap2.tsx
+++ assets/Maps/CustomTilesetMap2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="CustomTilesetMap2" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="../Terrain/CustomTilesetMap2.png" width="256" height="256"/>
+<tileset version="1.10" tiledversion="1.11.2" name="CustomTilesetMap2" tilewidth="16" tileheight="16" tilecount="1408" columns="22">
+ <image source="../Terrain/CustomTilesetMap2.png" width="352" height="1024"/>
 </tileset>
